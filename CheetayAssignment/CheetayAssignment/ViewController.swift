145b6a2ba7ba7b9b92383bd4294bfc70aa4647fe
usama farooq <usama.farooq@norig.com>
2023-07-07 15:43:46 +0500

    initial commit

---
+++ CheetayAssignment/CheetayAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheetayAssignment
+//
+//  Created by usama farooq on 07/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
